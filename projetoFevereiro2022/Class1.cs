ff9ed26f8f4ae4f8aa76d6ea996fbf44d636de6d
souza75 <lucianosouza75@gmail.com>
2022-02-27 12:42:04 -0300

    Adicionar arquivos de projeto.

---
+++ projetoFevereiro2022/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace projetoFevereiro2022
+{
+    public class Serie
+    {
+      
+        private String nomeSerie;
+        private String genero;
+     
+
+        public Serie(String nomeSerie, String genero)
+        {
+           
+            this.nomeSerie = nomeSerie;
+            this.genero = genero;
+         
+            
+        }
+
+
+
+        public String getNomeSerie()
+        {
+            return nomeSerie;
+        }
+
+        public void setNomeSerie()
+        {
+            this.nomeSerie = nomeSerie;
+        }
+
+        public String getGenero()
+        {
+            return genero;
+        }
+
+        public void setGenero()
+            {
+            this.genero= genero;
+        }
+
+      
+
+      override
+            public String toString()
+        {
+            return "Serie{" +  ", nomeSerie=" + nomeSerie + ", genero=" + genero +  '}';
+        }
+    }   
+}
---
+++ projetoFevereiro2022/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Serie;
+
+
+namespace Serie;
+{
+class Program
+{
+    private static string genero;
+
+    static void Main(string[] args, string nomeSerie, string genero)
+    {
+        while (true) {
+          
+            Console.WriteLine("Pesquisar série pelo nome");
+            Console.WriteLine("Pesquisar série pelo gênero");]
+            Console.WriteLine("Incluir série");
+            Console.WriteLine("Excluir série");
+
+            int opcao = Console.ReadLine("\nOpção: ");
+
+            switch (opcao) {
+                case 0: break;
+              
+
+                case 2: pesqSerieNome();
+                    break;
+
+                case 3: pesqSerieGenero();
+                    break;
+
+                case 4: incluirSerie;
+                    break;
+
+                case 5: excluirSerie;
+                    break;
+
+                default: Console.WriteLine("Opção invalida, escolha novamente");
+            }
+
+            public static void incluirSerie(string nomeSerie, string genero)
+            {
+                Console.WriteLine("Digite o nome da serie");
+                string nome = nomeSerie;
+                string g = genero;
+
+            }
+
+            public static void excluirSerie(string nomeSerie, string genero)
+            {
+                Console.WriteLine("Digite o nome da série a ser excluida");
+                string nome = nomeSerie.Trim();
+                genero = null;
+                nome = null;
+            }
+        }
+
+    }
+
+  
+
+    public static void pesqSerieNome()
+    {
+        Console.WriteLine("Pesquisar série pelo nome");
+        string v = Console.ReadLine("nome da Serie"+nomeSerie);
+        String nomeSerie = v;
+    }
+
+    public static void pesqSerieGenero()
+    {
+        Console.WriteLine("Pesquisar serie pelo Nome:");
+        string s = Console.ReadLine("genero da Serie" + genero);
+        String nomeSerie = s;
+    }
+}
